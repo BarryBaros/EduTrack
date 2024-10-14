b4015343c03d2aaadda83712cb04d1bfb6c77758
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-10-14 19:40:04 +0300

    added routes

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,22 @@
+import React from 'react';
+
 function Homepage(){
-    return
+    return (
+            < div className="app">
+                {/* siebar */}
+                <aside className='sidebar'>
+                    <ul>
+                        <li><a href='#' className='social-icon linkedln'>Linkedln</a></li>
+                        <li><a href='#' className='social-icon instagram'>Instagram</a></li>
+                        <li><a href='#' className='social-icon youtube'>YouTube</a></li>
+                    </ul>
+                </aside>
+
+                {/* main content */}
+
+            </div>
+     );
+    
 }
 
 export default Homepage;
