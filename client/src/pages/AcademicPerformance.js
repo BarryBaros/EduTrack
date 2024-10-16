440694d3030a8d46e3c5d3dd2f5dc82f6fd97564
matthewndirangu <ndirangumatthew9@gmail.com>
2024-10-16 19:40:07 +0300

    updated

---
+++ client/src/pages/AcademicPerformance.js
@@ -0,0 +1,13 @@
+// src/pages/AcademicPerformance.js
+import React from 'react';
+
+const AcademicPerformance = () => {
+  return (
+    <div>
+      <h1>Academic Performance</h1>
+      {/* Add grades table or charts here */}
+    </div>
+  );
+};
+
+export default AcademicPerformance;
