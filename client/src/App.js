622a6223581863f1fafa5528ee1e3e2d52694a6e
Barasa <timbaros87@gmail.com>
2024-10-17 20:58:57 +0300

    Update Teachers page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import Home from './components/Home';
 import Login from './components/Login';
 import About from './components/About';
@@ -24,26 +23,5 @@ const App = () => {
         </Router>
     );
 };
-=======
-import Login from './components/Login';
-import Homepage from './components/Homepage';  // Fix the import
-
-function App() {
-  const handleLogin = (admissionNumber, staffNumber, pinNumber) => {
-    console.log('Login Info:', admissionNumber, staffNumber, pinNumber);
-    // Perform login logic here
-  };
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login onLogin={handleLogin} />} /> {/* Fixed Route */}  // Changed to '/' as per the requirement
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route path="/homepage" element={<Homepage />} /> {/* Fixed Route */}
-      </Routes>
-    </Router>
-  );
-}
->>>>>>> 58e5de7873bebc93938c08cb6d2b95fd106a8ad6
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
 import './index.css';
-=======
-
->>>>>>> 58e5de7873bebc93938c08cb6d2b95fd106a8ad6
+import './App.css';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -13,8 +10,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 58e5de7873bebc93938c08cb6d2b95fd106a8ad6
